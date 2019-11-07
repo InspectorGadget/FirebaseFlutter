18046c3ee1eb7cf1352cba3ab03b2f6339de4c4c
InspectorGadget™ <igadget28@gmail.com>
2019-11-07 22:49:27 +0800

    completed authentication

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -2,6 +2,10 @@ import 'package:firebase/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
+
+  final Function toggleView;
+  Register({ this.toggleView });
+
   @override
   _RegisterState createState() => _RegisterState();
 }
@@ -9,9 +13,11 @@ class Register extends StatefulWidget {
 class _RegisterState extends State<Register> {
 
   final AuthService _authService = AuthService();
+  final _formKey = GlobalKey<FormState>();
   
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +27,25 @@ class _RegisterState extends State<Register> {
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
         title: Text('Sign up to App'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Sign in'),
+            onPressed: () {
+              widget.toggleView();
+            },
+          )
+        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (value) => value.isEmpty ? 'Enter an Email' : null,
                 onChanged: (value) {
                   setState(() {
                     email = value;
@@ -37,6 +54,7 @@ class _RegisterState extends State<Register> {
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (value) => value.length < 6 ? 'Enter a password with 6+ chars long' : null,
                 obscureText: true,
                 onChanged: (value) {
                   setState(() {
@@ -52,9 +70,23 @@ class _RegisterState extends State<Register> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _authService.registerWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() {
+                       error = 'Please supply valid email'; 
+                      });
+                    }
+                  }
                 },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0
+                ),
               )
             ],
           ),
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -2,6 +2,10 @@ import 'package:firebase/services/auth.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
+
+  final Function toggleView;
+  SignIn({ this.toggleView });
+
   @override
   _SignInState createState() => _SignInState();
 }
@@ -9,9 +13,11 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   
   final AuthService _authService = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   String email = '';
   String password = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -21,14 +27,25 @@ class _SignInState extends State<SignIn> {
         backgroundColor: Colors.brown[400],
         elevation: 0.0,
         title: Text('Sign in to App'),
+        actions: <Widget>[
+          FlatButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('Register'),
+            onPressed: () {
+              widget.toggleView();
+            },
+          )
+        ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an Email' : null,
                 onChanged: (value) {
                   setState(() {
                     email = value;
@@ -37,6 +54,7 @@ class _SignInState extends State<SignIn> {
               ),
               SizedBox(height: 20.0),
               TextFormField(
+                validator: (val) => val.length < 6 ? 'Enter a password with 6+ chars long' : null,
                 obscureText: true,
                 onChanged: (value) {
                   setState(() {
@@ -52,9 +70,23 @@ class _SignInState extends State<SignIn> {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {
+                    dynamic result = await _authService.signInWithEmailAndPassword(email, password);
+                    if (result == null) {
+                      setState(() {
+                       error = 'Could not sign in with those Credentials :('; 
+                      });
+                    }
+                  }
                 },
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 14.0
+                ),
               )
             ],
           ),
